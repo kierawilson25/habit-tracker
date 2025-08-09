61e424617f3915a620829f1b6aee362e0782bb32
Kiera Wilson <60899868+kierawilson25@users.noreply.github.com>
2025-08-08 21:13:30 -0400

    feat: add logout functionality (#73)
    Description: 
    - Add header component 
    - Add logout button and functionality in header

--- Front-end/src/app/layout.tsx
+++ Front-end/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AuthProvider } from '../context/AuthContext'
+import AppHeader from "../components/AppHeader";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,7 +30,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          {children}
+          <AppHeader />
+          <main className="pt-16"> 
+            {children}
+          </main>
         </AuthProvider>
       </body>
     </html>
