2612a9039ade8fdbb794dff73a16b70db0d0e746
kierawilson25 <60899868+kierawilson25@users.noreply.github.com>
2025-11-23 12:56:37 -0800

    refactor(components/streakCalendar): turn streak calendar into a component

--- Front-end/src/components/index.tsx
+++ Front-end/src/components/index.tsx
@@ -3,4 +3,5 @@ export { default as AppHeader } from './AppHeader';
 export { default as Button } from './Button';
 export { default as Footer } from './Footer';
 export { default as H1 } from './H1';
+export { default as StreakCalendar } from './StreakCalendar';
 export { default as TextBox } from './TextBox';
